30d6958505729f3ef6c1fee49f5662c73dcc7039
김수현 <kimsuhyeon@gimsuhyeon-ui-MacBookPro.local>
2025-08-08 08:21:21 +0900

    build : 빌드 파일 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
 
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
+    base: "/front_6th_chapter2-2/",
+    build: {
+      rollupOptions: {
+        input: "index.advanced.html",
+      },
+    },
   }),
   defineTestConfig({
     test: {
