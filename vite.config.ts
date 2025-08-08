5cf5da792df7b9078c0acd988b6e70c02a9c02e4
김수현 <kimsuhyeon@gimsuhyeon-ui-MacBookPro.local>
2025-08-08 10:03:44 +0900

    fix : 빌드 설정 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,38 @@
-import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
-import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import fs from "fs";
+import path from "path";
+import { defineConfig } from "vitest/config";
 
-export default mergeConfig(
-  defineConfig({
-    plugins: [react()],
-    base: "/front_6th_chapter2-2/",
-    build: {
-      rollupOptions: {
-        input: "index.advanced.html",
+const base =
+  process.env.NODE_ENV === "production" ? "/front_6th_chapter2-2/" : "";
+const entryFileName = "index.advanced.html";
+
+export default defineConfig({
+  base,
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src/advanced"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: path.resolve(__dirname, entryFileName),
+    },
+  },
+  plugins: [
+    react() as any,
+    {
+      name: "rename-html-output",
+      closeBundle() {
+        const from = path.resolve(__dirname, `dist/${entryFileName}`);
+        const to = path.resolve(__dirname, "dist/index.html");
+        if (fs.existsSync(from)) fs.renameSync(from, to);
       },
     },
-  }),
-  defineTestConfig({
-    test: {
-      globals: true,
-      environment: "jsdom",
-      setupFiles: "./src/setupTests.ts",
-    },
-  })
-);
+  ],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "src/setupTests.js",
+  },
+});
